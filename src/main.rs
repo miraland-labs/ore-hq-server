ff968e3493ff8d87d04ea41ab1d9af9b87e8d3fa
Miraland Labs <miraland.labs@gmail.com>
2024-08-20 08:46:09 +0800

    change should mine to false once time cut off

--- src/main.rs
+++ src/main.rs
@@ -389,10 +389,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let cutoff = get_cutoff(&rpc_client, proof, *app_buffer_time).await;
             let mut should_mine = true;
             let cutoff = if cutoff <= 0 {
-                let solution = app_epoch_hashes.read().await.best_hash.solution;
-                if solution.is_some() {
-                    should_mine = false;
-                }
+                // MI, vanilla
+                // let solution = app_epoch_hashes.read().await.best_hash.solution;
+                // if solution.is_some() {
+                //     should_mine = false;
+                // }
+                should_mine = false; // MI
                 0
             } else {
                 cutoff
